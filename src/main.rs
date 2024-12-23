0d8ab010ac5e9dabdbb403661683e02e0398c080
ammar siddiqui <ammar.siddiqui847@gmail.com>
2024-12-22 19:06:37 -0500

    added actix web and baseline config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+use dotenvy::dotenv;
+
+mod config;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let config = config::Config::from_env();
+
+    HttpServer::new(move || {
+        App::new().route("/", actix_web::web::get().to(|| async { "Hello, Actix!" }))
+    })
+    .bind((config.host, config.port))?
+    .run()
+    .await
 }
