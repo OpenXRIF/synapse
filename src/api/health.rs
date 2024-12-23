32a0dac74e0adc296b32d6dd14e63e386c1c7b2a
ammar siddiqui <ammar.siddiqui847@gmail.com>
2024-12-22 19:19:09 -0500

    api v1 setup

--- src/api/health.rs
+++ src/api/health.rs
@@ -1,1 +1,11 @@
+use actix_web::{web, HttpResponse, Scope};
 
+pub fn configure(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::scope("/health").route("", web::get().to(health_check)));
+}
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().json(serde_json::json!({
+        "status": "Healthy"
+    }))
+}
