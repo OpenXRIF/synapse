60cf36eb8efdf360e10f19c9a72c8e90c67a6a10
ammar siddiqui <ammar.siddiqui847@gmail.com>
2024-12-25 12:15:16 -0500

    updated base structs

--- src/inference/base.rs
+++ src/inference/base.rs
@@ -1,19 +1,14 @@
-use std::any::Any;
 use std::collections::HashMap;
+use std::hash::Hash;
 use std::str::FromStr;
-use std::sync::Mutex;
 
-use crate::inference::interfaces::{
-    cohere_interface::CohereStrategy, local_interface::LocalStrategy,
-};
-
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Modality {
     Text,
     ImageText,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Hash, Clone, Eq, PartialEq)]
 pub enum ModelProvider {
     Cohere, // Models from Cohere API
     Custom, // Models from Axon (ML platform)
@@ -38,60 +33,24 @@ pub struct ModelConfig {
     pub name: String,
     pub provider: ModelProvider,
     pub modality: Modality,
-    pub config_params: Option<HashMap<String, Any>>,
-
-    // Private params
-    api_key: Option<String>,
+    pub api_key: Option<String>,
+    pub config_params: Option<HashMap<String, String>>,
 }
 
-pub trait ModelStrategy {
-    fn new(config: ModelConfig) -> Self
-    where
-        Self: Sized;
-
-    fn prompt(...) -> Any;
-}
-
-pub struct ModelStrategyFactory {
-    strategies: Mutex<HashMap<ModelProvider, Arc<dyn Fn(ModelConfig) -> Box<dyn ModelStrategy>>>>,
-}
-
-impl ModelStrategyFactory {
-    pub fn new() -> Self {
-        let mut strategies: HashMap<
-            ModelProvider,
-            Arc<dyn Fn(ModelConfig) -> Box<dyn ModelStrategy>>,
-        > = HashMap::new();
-
-        strategies.insert(
-            ModelProvider::Cohere,
-            Arc::new(|config| Box::new(CohereStrategy::new(config))),
-        );
-        strategies.insert(
-            ModelProvider::Local,
-            Arc::new(|config| Box::new(LocalStrategy::new(config))),
-        );
-
-        ModelStrategyFactory {
-            strategies: Mutex::new(strategies),
+impl ModelConfig {
+    pub fn new(
+        name: String,
+        provider: ModelProvider,
+        modality: Modality,
+        config_params: Option<HashMap<String, String>>,
+        api_key: Option<String>,
+    ) -> Self {
+        ModelConfig {
+            name,
+            provider,
+            modality,
+            config_params,
+            api_key,
         }
-    }
-
-    pub fn register_strategy<F>(&self, provider: ModelProvider, strategy_fn: F)
-    where
-        F: Fn(ModelConfig) -> Box<dyn ModelStrategy> + 'static,
-    {
-        self.strategies
-            .lock()
-            .unwrap()
-            .insert(provider, Box::new(strategy_fn));
-    }
-
-    pub fn create_strategy(&self, config: ModelConfig) -> Box<dyn ModelStrategy> {
-        let strategies = self.strategies.lock().unwrap();
-        let strategy_fn = strategies
-            .get(&config.provider)
-            .expect("Unsupported model provider");
-        strategy_fn(config)
     }
 }
