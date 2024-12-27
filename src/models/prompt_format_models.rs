ec02d340160b215819f5d2b961f36916b270845b
ammar siddiqui <ammar.siddiqui847@gmail.com>
2024-12-27 09:28:58 -0500

    enabled prompt format arg types

--- src/models/prompt_format_models.rs
+++ src/models/prompt_format_models.rs
@@ -3,18 +3,22 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub enum PromptFormatArg {
-    String(String),
-    Integer(i32),
-    Float(f32),
-    Boolean(bool),
+pub enum PromptFormatArgType {
+    #[serde(rename = "string")]
+    String,
+    #[serde(rename = "number")]
+    Number,
+    #[serde(rename = "boolean")]
+    Boolean,
+    #[serde(rename = "list")]
+    List,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PromptFormat {
     pub prompt_format_name: String,
     pub prompt: String,
-    pub prompt_args: HashMap<String, PromptFormatArg>,
+    pub prompt_args: HashMap<String, PromptFormatArgType>,
     pub rag_args: HashMap<String, String>,
     pub metadata: HashMap<String, String>,
 }
